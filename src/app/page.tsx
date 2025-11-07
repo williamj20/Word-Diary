638d7cf72edd685e71c90500d26d6868dc714f98
williamj20 <williamjiang20@gmail.com>
2025-11-06 20:13:12 -0500

    add prettier and icon to title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import { BookOpenText } from 'lucide-react';
+
 export default function Home() {
   return (
     <div className="flex items-center flex-col">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1 className="font-semibold text-5xl mt-8">Word Diary</h1>
-      </main>
+      <div className="flex gap-3 items-center mt-3">
+        <BookOpenText className="w-10 h-10" />
+        <h1 className="font-semibold text-5xl">Word Diary</h1>
+      </div>
     </div>
   );
 }
