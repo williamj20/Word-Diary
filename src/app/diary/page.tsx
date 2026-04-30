8c527fabd24d9444b0536f7edfba74ad1335387e
William Jiang <williamjiang20@gmail.com>
2026-04-30 13:31:21 -0400

    Add suspense to search bar + word list

--- src/app/diary/page.tsx
+++ src/app/diary/page.tsx
@@ -2,6 +2,7 @@ import AddWord from '@/app/diary/components/add-word';
 import SavedWordsSearch from '@/app/diary/components/saved-words-search';
 import WordList from '@/app/diary/components/word-list';
 import { redirectToSignupIfNotLoggedIn } from '@/app/lib/utils';
+import { Suspense } from 'react';
 
 const normalizeSearchParam = (value?: string | string[]) => {
   if (Array.isArray(value)) {
@@ -28,8 +29,12 @@ const DiaryPage = async (props: {
           YOUR SAVED WORDS
         </h2>
         <div className="mt-3 h-px w-36 bg-gradient-to-r from-transparent via-[var(--brass)] to-transparent" />
-        <SavedWordsSearch />
-        <WordList query={q} />
+        <Suspense>
+          <SavedWordsSearch />
+        </Suspense>
+        <Suspense key={q}>
+          <WordList query={q} />
+        </Suspense>
       </div>
     </main>
   );
