751d405cd9231336c8e707e58edc347e8b691066
William Jiang <williamjiang20@gmail.com>
2026-05-09 13:07:59 -0400

    Add local dev singleton to db client

--- src/app/lib/dbClient.ts
+++ src/app/lib/dbClient.ts
@@ -1,5 +1,22 @@
 import postgres from 'postgres';
 
-const sql = postgres(process.env.DATABASE_URL!);
+const globalForSql = globalThis as unknown as {
+  sql: postgres.Sql | undefined;
+};
+
+if (globalForSql.sql) {
+  console.log('Reusing existing postgres connection');
+}
+
+const sql =
+  globalForSql.sql ??
+  (() => {
+    console.log('Creating new postgres connection');
+    return postgres(process.env.DATABASE_URL!);
+  })();
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForSql.sql = sql;
+}
 
 export default sql;
