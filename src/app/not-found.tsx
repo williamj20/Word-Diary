c1a040a8cc1b18bf20ab288e44cf2744620eb1f5
William Jiang <williamjiang20@gmail.com>
2026-05-10 22:06:47 -0400

    Increase responsiveness on small width screens for signup/login and notfound

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,14 +2,16 @@ import AppHeader from '@/app/components/app-header';
 
 const NotFound = () => {
   return (
-    <>
-      <AppHeader showAuthButtons={false} />
-      <div className="flex items-center justify-center">
-        <h2 className="display-font text-md font-bold uppercase text-[var(--ink)]">
-          Page Not Found
-        </h2>
+    <div className="overflow-x-auto">
+      <div className="mx-auto min-w-xs px-3">
+        <AppHeader showAuthButtons={false} />
+        <div className="flex items-center justify-center">
+          <h2 className="display-font text-md font-bold uppercase text-[var(--ink)]">
+            Page Not Found
+          </h2>
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
